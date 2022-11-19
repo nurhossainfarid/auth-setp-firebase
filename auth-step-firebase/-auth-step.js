52c5e1817475ea7a636ef267607d6e74d88062c3
nurhossainfarid <faahsan1516@gmail.com>
2022-11-19 23:32:59 +0600

    first commit

---
+++ auth-step-firebase/-auth-step.js
@@ -0,0 +1,23 @@
+/* 
+    1. create a new project in console.firebase.google.com
+    2. npm install firebase
+    3. create firebase.init.js and import getAuth to export auth
+    4. firebase>authentication>enable email and password
+    5. create login and signup components, set route
+    6. attach from field handler and from submit handler
+    7. npm install --save react-firebase-hooks
+    8. useCreateUserWithEmailAndPassword from react firebase hooks
+    9. user is true go to user choices page
+    10. useSignInWithEmailAndPassword for Login
+    11. create RequireAuth component ==> check user exist also tract user location
+    12. In route wrap protected component by using RequireAuth Component
+*/
+
+/**
+    hosting steps
+     * npm install -g firebase-tools
+     * firebase login
+     * firebase init
+     * npm run build
+     * firebase deploy
+*/
